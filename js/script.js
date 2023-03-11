16e3d7fa92fb4d0173d0daf9ae00ba1e99c40d7b
Roderen <mikilikala@gmail.com>
2023-03-11 23:30:55 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
 "use strict";
+
+function equals(a, b) {
+  console.log(Boolean(a === b));
+}
+equals(1, 1);
+
+function compare(a, b) {
+  console.log(Boolean((a + b) > 10))
+}
+compare(1, 1)
+
+function checkBoolean(a) {
+  console.log(Boolean(a < 0))
+}
+checkBoolean(-1);
+
+
+
+// function equals(a, b) {
+//   if (a === b) {
+//     console.log(true)
+//   } else {
+//     console.log(false)
+//   }
+// }
+// equals(1, 1);
+//
+// function compare(a, b) {
+//   if (a + b > 10) {
+//     console.log(true)
+//   } else {
+//     console.log(false)
+//   }
+// }
+// compare(1, 1)
+//
+// function checkBoolean(a) {
+//   if (a < 0) {
+//     console.log(true)
+//   } else {
+//     console.log(false)
+//   }
+// }
+// checkBoolean(-1);
