676c649c59f4659eba2edbb7bf1c664019ada57c
Roderen <mikilikala@gmail.com>
2023-03-12 10:51:58 +0200

    changed the return func

--- js/script.js
+++ js/script.js
@@ -1,45 +1,19 @@
 "use strict";
 
 function equals(a, b) {
-  console.log(Boolean(a === b));
+  return Boolean(a === b);
 }
-equals(1, 1);
+
+console.log(equals(1, 1));
 
 function compare(a, b) {
-  console.log(Boolean((a + b) > 10))
+  return Boolean((a + b) > 10)
 }
-compare(1, 1)
+
+console.log(compare(1, 1));
 
 function checkBoolean(a) {
-  console.log(Boolean(a < 0))
+  return Boolean(a < 0)
 }
-checkBoolean(-1);
 
-
-
-// function equals(a, b) {
-//   if (a === b) {
-//     console.log(true)
-//   } else {
-//     console.log(false)
-//   }
-// }
-// equals(1, 1);
-//
-// function compare(a, b) {
-//   if (a + b > 10) {
-//     console.log(true)
-//   } else {
-//     console.log(false)
-//   }
-// }
-// compare(1, 1)
-//
-// function checkBoolean(a) {
-//   if (a < 0) {
-//     console.log(true)
-//   } else {
-//     console.log(false)
-//   }
-// }
-// checkBoolean(-1);
+console.log(checkBoolean(-1))
