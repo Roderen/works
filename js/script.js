bb7116ebadac032d1c67da48c6e9306c1455626e
Roderen <mikilikala@gmail.com>
2023-03-14 23:11:25 +0200

    Added switch case

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 "use strict";
 
+// #1
 const numOrStr = prompt('input number or string');
 numOrStr === null || numOrStr.trim() === '' || isNaN(+numOrStr) ? console.log('Error') : console.log('Success');
+
+
+// #2
+switch (numOrStr && numOrStr.trim() && isNaN(+numOrStr)) {
+  case null:
+    console.log('Null')
+    break;
+
+  case '':
+    console.log('Empty string')
+    break;
+
+  case true:
+    console.log('NaN')
+    break;
+
+  default:
+    console.log('Success')
+    break;
+}
