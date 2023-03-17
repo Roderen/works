02d3b89065e40bf3da50a6b7e4cc461281f5805b
Roderen <mikilikala@gmail.com>
2023-03-17 09:42:10 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+// #1
+// let x = 10;
+// let y = 7;
+//
+// x > y ? console.log('x больше, чем y') : console.log('x не больше, чем y')
+
+
+
+// #2
+// const num = parseInt(prompt('Введите число'));
+//
+// if (num % 2 === 0) {
+//   console.log(`Число ${num} четное`);
+// } else {
+//   console.log(`Число ${num} нечетное`);
+// }
+
+
+
+// #3
+const numTwo = parseInt(prompt('Введите целое число'));
