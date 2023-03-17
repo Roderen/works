b69c301fd9819a5be52010e8878867d932357aba
Roderen <mikilikala@gmail.com>
2023-03-17 11:22:33 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const number = parseInt(prompt('Введите число', '3245'));
+
+if (number >= 0) {
+  let lastNumber = number.toString().slice(-1);
+  lastNumber = Number(lastNumber);
+
+  if (lastNumber === 1) {
+    alert(`${number} год`);
+  } else if (lastNumber >= 2 && lastNumber <= 4) {
+    alert(`${number} года`);
+  } else {
+    alert(`${number} лет`);
+  }
+} else {
+  alert('Это не число!');
+}
