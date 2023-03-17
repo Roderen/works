a45ad13e075478ff24a67dd30d8aefe906fc7f5b
Roderen <mikilikala@gmail.com>
2023-03-17 10:39:39 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+const dateOfBirth = parseInt(prompt('Год рождения', '1995'));
+const aboutYou = {};
+
+if (dateOfBirth) {
+  aboutYou.date = dateOfBirth;
+  const yourCity = prompt('В каком городе ты живешь?', 'Киев');
+
+  if (yourCity) {
+    switch (yourCity) {
+      case 'Киев':
+      case 'Москва':
+      case 'Минск':
+        aboutYou.capital = yourCity;
+        break;
+      default:
+        aboutYou.city = yourCity;
+    }
+
+    const typeOfSport = prompt('Твой любимый вид спорта?', 'Теннис');
+
+    switch (typeOfSport) {
+      case 'Теннис':
+      case 'Футбол':
+      case 'Баскетбол':
+        aboutYou.sport = typeOfSport;
+
+        alert();
+
+        break;
+      default:
+        alert(`Ты не захотел вводить свой вид спорта`);
+    }
+  } else {
+    alert('Ты не захотел вводить свой город');
+  }
+} else {
+  alert('Ты не захотел вводить год рождения');
+}
