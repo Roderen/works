869b08bf2f400346a02f472e2ee0c1bf62b14a8a
Roderen <mikilikala@gmail.com>
2023-03-22 12:52:03 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const arr = [1, 2, 3, -1, -2, -3];
+function returnArray(array) {
+  const resultArr = []
+  if (!array.length) return 'Массив пуст';
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= 1) resultArr.push(array[i]);
+  }
+
+  if (!resultArr.length) return null;
+  return resultArr;
+}
+
+alert(returnArray(arr))
