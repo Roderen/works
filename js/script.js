ed9f4e2d632da70c09ad6f0145cc70ecef056ed5
Roderen <mikilikala@gmail.com>
2023-03-27 22:50:19 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const number = parseInt(prompt('Number', '11'))
+
+if (number <= 1) alert('NaN');
+if (number > 1) {
+  let i = 2;
+  while (number % i !== 0) {
+    i++;
+  }
+  if (number === i) alert(`Число ${number} простое`);
+
+  /* Не могу понять почему не получается сделать через for... ¯\_(ツ)_/¯ */
+  // for (let i = 2; number % i !== 0; i++) {
+  //   if (number === i) {
+  //     alert(number)
+  //   }
+  // }
+}
