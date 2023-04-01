782d93c4123f8b06a770c3c673035f8310060457
Roderen <mikilikala@gmail.com>
2023-04-02 01:07:35 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const number = parseInt(prompt('Число'))
+
+if (number <= 1) alert('Число должно быть больше единицы')
+if (isNaN(number)) alert('is NaN')
+if (number === 2) alert('Простое')
+if (number > 2) {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      alert('false')
+      break
+    }
+    alert('true')
+    break
+  }
+}
+
+// const number = parseInt(prompt('Число'))
+//
+// const calc = (n) => {
+//   if (n <= 1) return 'Число должно быть больше единицы'
+//   if (isNaN(n)) return 'is NaN'
+//   if (n === 2) return 'Простое'
+//   if (n > 2) {
+//     for (let i = 2; i < n; i++) {
+//       if (n % i === 0) {
+//         return 'false'
+//       }
+//       return 'true'
+//     }
+//   }
+// }
+// alert(calc(number))
