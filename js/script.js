01f8128c807b9e65427c1b79212ac920fd656c3e
Roderen <mikilikala@gmail.com>
2023-04-14 20:41:52 +0300

    added eslint, prettier

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const a = 10;
+const b = 2;
+
+function sum(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(sum(a, b));
+console.log(minus(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
