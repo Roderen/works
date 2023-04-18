06df75806686146f19e7b94d855b5f05f829e64c
Roderen <mikilikala@gmail.com>
2023-04-18 13:33:24 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const mainFunction = (callback) => {
+  const a = +prompt("Число:");
+  const b = +prompt("Степень:");
+  callback(a, b);
+};
+
+const exponentiation = (a, b) => {
+  const result = a ** b;
+  alert(result);
+};
+
+mainFunction(exponentiation);
