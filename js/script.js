d57aa1e76c3a05b1716c75a31b15ceeb59943ca8
Roderen <mikilikala@gmail.com>
2023-04-19 01:18:59 +0300

    remove boolean value

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 "use strict";
 
 function equals(a, b) {
-  return Boolean(a === b);
+  return a === b;
 }
 
 console.log(equals(1, 1));
 
 function compare(a, b) {
-  return Boolean((a + b) > 10)
+  return (a + b) > 10
 }
 
 console.log(compare(1, 1));
 
 function checkBoolean(a) {
-  return Boolean(a < 0)
+  return a < 0
 }
 
 console.log(checkBoolean(-1))
