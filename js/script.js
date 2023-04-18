3c6353e62dba6ca0313cfb3a49b079a31a4e0569
Roderen <mikilikala@gmail.com>
2023-04-19 01:09:01 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+// #1
+const user1 = {};
+user1.name = 'John';
+user1.surname = 'Smith';
+user1.name = 'Pete';
+delete user1.name;
+
+// #2
+const user2 = {
+  name: "John"
+};
+user2.name = "Pete";
+
+
+// #3
+let sum = null;
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+for (let i of Object.values(salaries)) {
+  sum = sum + i;
+}
