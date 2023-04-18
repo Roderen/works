928af84c8d28c29566482c9963ab71c16659eab7
Roderen <mikilikala@gmail.com>
2023-04-19 00:45:16 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const x = 4;
+const y = 9;
+const radius = 10;
+const hypotenuse = Math.sqrt(x ** 2 + y ** 2);
+
+hypotenuse <= radius ? alert('Точка внутри окружности') : alert('Точка за пределами круга');
