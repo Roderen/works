75bf7adb33ce2d169eba0ba404ac3e53cee00d1d
Roderen <mikilikala@gmail.com>
2023-04-19 02:02:24 +0300

    added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 "use strict";
 
-const number = parseInt(prompt('Введите число', '3245'));
+const number = parseInt(prompt('Введите число', '12'));
 
-if (number >= 0) {
-  let lastNumber = number.toString().slice(-1);
-  lastNumber = Number(lastNumber);
-
-  if (lastNumber === 1) {
-    alert(`${number} год`);
-  } else if (lastNumber >= 2 && lastNumber <= 4) {
-    alert(`${number} года`);
-  } else {
-    alert(`${number} лет`);
-  }
-} else {
-  alert('Это не число!');
-}
+const ageFunc = (number) => {
+  if (isNaN(number)) return "Это не число";
+  if (number < 0) return "Это отрицательное число";
+  if (number % 10 === 1 || number === 0) return `${number} год`;
+  if (number % 100 >= 5 && number % 100 <= 20) return `${number} лет`;
+  if (number % 10 >= 2 && number % 10 <= 4) return `${number} года`;
+  return `${number} лет`;
+};
+console.log(ageFunc(number));
