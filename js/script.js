fc2bb7db7d8ade248356c00af4d4599f55866e43
Roderen <mikilikala@gmail.com>
2023-04-19 01:01:12 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const fid = (n = 1) => {
+  let prev = 0;
+  let next = 1;
+  for(let i = 0; i < n; i++){
+    next = prev + next;
+    prev = next - prev;
+  }
+  return prev;
+};
+console.log(fid(100));
