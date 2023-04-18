5318b2a2e385828f2979dc2e060f642d7333a35c
Roderen <mikilikala@gmail.com>
2023-04-19 01:20:06 +0300

    i

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ function equals(a, b) {
 console.log(equals(1, 1));
 
 function compare(a, b) {
-  return (a + b) > 10
+  return (a + b) > 10;
 }
 
 console.log(compare(1, 1));
 
 function checkBoolean(a) {
-  return a < 0
+  return a < 0;
 }
 
-console.log(checkBoolean(-1))
+console.log(checkBoolean(-1));
