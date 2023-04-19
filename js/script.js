17f1b8a7a844280f43638986656f36e20d2ca7b2
Roderen <mikilikala@gmail.com>
2023-04-20 01:55:14 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 "use strict";
+(function () {
+  // #1
+  function getFactorial(n) {
+    if (n === 1 || n === 0) return 1;
+    return n * getFactorial(n - 1);
+  }
+
+  console.log(getFactorial(3));
+
+  // #2
+  function getPow(num, deg) {
+    if (deg === 1) return num;
+    return num * getPow(num, deg - 1);
+  }
+
+  console.log(getPow(3, 2));
+
+  // #3
+  function getSum(a, b) {
+    if (b === 0) return a;
+    if (a === 0) return b;
+    let c = getSum(a - 1, b);
+    return ++c;
+  }
+
+  console.log(getSum(10, 5));
+})();
