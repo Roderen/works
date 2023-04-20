8e4b898db367026d752ab6afb75d7d9ce9be1aa6
Roderen <mikilikala@gmail.com>
2023-04-20 22:30:00 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
 "use strict";
 (function () {
+  function checkPalindrome(num) {
+    if (num < 0) {
+      num = Math.abs(num);
+    }
 
+    let palindromeResult = {
+      result: 0,
+      steps: 0,
+    };
+
+    function iterNum(num2) {
+      let reverseNum = +num2.toString().split("").reverse().join("");
+
+      if (num2 === reverseNum) {
+        palindromeResult.result = num2;
+        return num2;
+      }
+
+      if (num2 !== reverseNum) {
+        num2 = num2 + reverseNum;
+        palindromeResult.steps++;
+        iterNum(num2);
+      }
+    }
+
+    iterNum(num);
+    return palindromeResult;
+  }
+
+  console.log(checkPalindrome(31153));
 })();
