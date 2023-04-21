2419aac432e525c75a9794a6d0560ac3115f025d
Roderen <mikilikala@gmail.com>
2023-04-22 00:14:18 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 "use strict";
 (function () {
+  function sum(num = 0) {
+    function increse(increaseNum) {
+      num += increaseNum;
+      return num;
+    }
+    return increse;
+  }
 
+  const result = sum();
+  result(3);
+  result(5);
+  result(20);
 })();
