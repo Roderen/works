29e6262cd6bdd342f84dcb7740a7ab7be81b6000
Roderen <mikilikala@gmail.com>
2023-04-21 04:16:59 +0300

    rename const

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
 "use strict";
 (function () {
   function checkPalindrome(num) {
-    if (num < 0) {
-      num = Math.abs(num);
-    }
+    if (num < 0) num = Math.abs(num);
 
     let palindromeResult = {
       result: 0,
       steps: 0,
     };
 
-    function iterNum(num2) {
-      const reverseNum = +num2.toString().split("").reverse().join("");
+    function checkPalindromeInner(numInner) {
+      const reverseNum = +numInner.toString().split("").reverse().join("");
 
-      if (num2 === reverseNum) {
-        palindromeResult.result = num2;
-        return num2;
+      if (numInner === reverseNum) {
+        palindromeResult.result = numInner;
+        return numInner;
       }
 
-      if (num2 !== reverseNum) {
-        num2 = num2 + reverseNum;
+      if (numInner !== reverseNum) {
+        numInner = numInner + reverseNum;
         palindromeResult.steps++;
-        iterNum(num2);
+        checkPalindromeInner(numInner);
       }
     }
 
-    iterNum(num);
+    checkPalindromeInner(num);
     return palindromeResult;
   }
 
