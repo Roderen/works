86c38916637807459b3adb8683b6d4ab6e80f0bd
Roderen <mikilikala@gmail.com>
2023-04-22 01:15:24 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 "use strict";
 (function () {
+  function generateNum(num = []) {
+    function generateNumInner() {
+      if (num.length > 100) return num;
+      const randomNumber = Math.floor(Math.random() * 100);
+      if (num.indexOf(randomNumber) !== -1) {
+        return generateNumInner();
+      }
+      num.push(randomNumber);
+      return num;
+    }
 
+    return generateNumInner;
+  }
+  const result = generateNum();
+  result();
+  result();
+  result();
+  result();
+  console.log(result());
 })();
