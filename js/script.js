f392413697db4c28dfc7d955cba5f557312a4014
Roderen <mikilikala@gmail.com>
2023-04-22 16:52:43 +0300

    edited

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 
 (function () {
   function flatMethod(arr) {
-    if (arguments.length === 0) throw new Error('Function accepts 1 argument');
-    if (arguments.length > 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
+    if (arguments.length === 0) throw new Error("Function accepts 1 argument");
+    if (arguments.length > 1) throw new Error("Function accepts only 1 argument, too much arguments provided");
 
     let newArray = [];
 
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
-        newArray = [...newArray, ...(flatMethod(arr[i]))];
+        newArray = [...newArray, ...flatMethod(arr[i])];
       } else {
         if (arr[i] != null) newArray.push(arr[i]);
       }
