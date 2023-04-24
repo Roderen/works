40d2e48935bb869fe20825a1ed39fa4fc9c23470
Roderen <mikilikala@gmail.com>
2023-04-24 07:12:52 +0300

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": "eslint:recommended",
-  "overrides": [],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: "module",
   },
-  "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"]
-  }
-}
+  rules: {
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+  },
+};
--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
 "use strict";
 (function () {
+  function counter(number = 0) {
+    const resultCounter = {
+      increase: 0,
+      decrease: 0,
+      value: 0,
+    };
 
+    return {
+      increase() {
+        number++;
+        return resultCounter.increase++;
+      },
+      decrease() {
+        number++;
+        return resultCounter.decrease--;
+      },
+      value() {
+        resultCounter.value++;
+        return number;
+      },
+      getStatistic() {
+        return resultCounter;
+      },
+      reset() {
+        for (const key in resultCounter) {
+          resultCounter[key] = 0;
+        }
+        number = 0;
+        return resultCounter;
+      },
+    };
+  }
+  const result = counter();
+  console.log(result);
 })();
