5386ff32915d6a6466ce0381ee126eab5448772e
Roderen <mikilikala@gmail.com>
2023-04-24 08:34:25 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 "use strict";
 (function () {
-
+  let calculator = {
+    a: 0,
+    b: 0,
+    read: function () {
+      this.a = +prompt("Первое число"),
+      this.b = +prompt("Второе число");
+      if (isNaN(this.a) || isNaN(this.b)) throw new Error("It's not a number");
+      if (this.a === null || this.b === null) throw new Error("You need to enter a number");
+    },
+    sum: function () {
+      return this.a + this.b;
+    },
+    mul: function () {
+      return this.a * this.b;
+    },
+  };
+  calculator.read();
+  console.log(calculator.sum());
+  console.log(calculator.mul());
 })();
