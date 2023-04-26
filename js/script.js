393c4b82acee9aff695b84efafaca61bb7ac0384
Roderen <mikilikala@gmail.com>
2023-04-26 15:21:02 +0300

    added validation for an empty string

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 "use strict";
 (function () {
   let calculator = {
-    a: 0,
-    b: 0,
-    read: function () {
-      this.a = +prompt("Первое число"),
-      this.b = +prompt("Второе число");
+    a: null,
+    b: null,
+    read () {
+      let numOne = prompt('First number');
+      let numTwo = prompt('Second number');
+      if (numOne.trim() === '' || numTwo.trim() === '') throw new Error("It's empty string");
+      this.a = Number(numOne.replace(/ /g, ''));
+      this.b = Number(numTwo.replace(/ /g, ''));
       if (isNaN(this.a) || isNaN(this.b)) throw new Error("It's not a number");
-      if (this.a === null || this.b === null) throw new Error("You need to enter a number");
     },
     sum: function () {
       return this.a + this.b;
