924c67b356ce0a598fddc48283ae7211e45e8ab0
Roderen <mikilikala@gmail.com>
2023-04-27 17:15:38 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 "use strict";
 (function () {
+  const list = document.querySelectorAll('.ulClass li');
 
+  // #1
+  for (let i of list) {
+    console.log(i);
+  }
+
+  // #2
+  console.log(list.length);
+
+  // #3
+  const arr = [];
+  list.forEach(item => {
+    arr.push(item.innerText);
+  })
+  console.log(arr);
 })();
