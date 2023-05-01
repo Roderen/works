4b0f77b39a8afaa7c9d675fb0736f7c1186cbb5a
Roderen <mikilikala@gmail.com>
2023-05-01 23:54:34 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 "use strict";
 (function () {
-
+  const cost = () => {
+    const amountPotato = 4;
+    const potatoWeight = 75;
+    const liters = 48;
+    const perKg = 13;
+    return Math.ceil(amountPotato * potatoWeight * liters / 1000 * perKg)
+  }
+  console.log(cost());
 })();
