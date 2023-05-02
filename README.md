40d9ee7517cb83d1460d75ede32ac8b69a2eac79
Roderen <mikilikala@gmail.com>
2023-05-02 16:30:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# ДЗ 37. This V3
+
+<hr>
+
+Написать свою реализацию
+функции <a href="https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Function/bind" target="_blank">
+bind</a>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 "use strict";
 (function () {
+  const info = {
+    name: 'Vova',
+    age: '26',
+  }
 
+  function fn(surname) {
+    return `${this.name}, ${this.age}, ${surname}`;
+  }
+
+  function myBind(fn, context, ...rest) {
+    return function () {
+      return fn.apply(context, rest);
+    }
+  }
+
+  const result = myBind(fn, info, 'Shaitan');
+  console.log(result());
 })();
