d4dfdf98a85756d354d80d27e49cf74f3bd6dbce
Roderen <mikilikala@gmail.com>
2023-05-02 17:06:58 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,41 +1,30 @@
 "use strict";
+(function () {
 
-const indexOfMethod = (arr, el, from = 0) => {
-  if (arr.constructor !== Array) throw new TypeError('This element is not an array')
-  if (from >= arr.length - 1) return -1
-  if (from < 0) {
+  const items = ['a', 'b', 'c', 'd', 'e', 'f']
+
+  const indexOfMethod = (arr, el, from = 0) => {
+    if (arr.constructor !== Array) throw new TypeError('This element is not an array')
+    if (from >= arr.length - 1) return -1
+    if (from < 0) {
+      for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === el) return i
+      }
+    }
+    for (let i = from; i < arr.length; i++) {
+      if (arr[i] === el) return i
+    }
+  }
+// console.log(indexOfMethod(items, 'e'))
+
+
+  const lastIndexOfMethod = (arr, el, from = 0) => {
+    if (arr.constructor !== Array) throw new TypeError('This element is not an array')
+    if (from >= arr.length - 1) return -1
     for (let i = arr.length - 1; i >= 0; i--) {
       if (arr[i] === el) return i
     }
   }
-  for (let i = from; i < arr.length; i++) {
-    if (arr[i] === el) return i
-  }
-}
-const items = ['a', 'b', 'c', 'd', 'e', 'f']
-// console.log(indexOfMethod(items, 'e'))
+// console.log(lastIndexOfMethod(items, 'e'))
 
-
-
-// const lastIndexOfMethod = (arr, el, from = 0) => {
-//   if (arr.constructor !== String) throw new Error('This element is not an array')
-//   if (from >= arr.length - 1) return -1
-//   let lastElementIndex = 0
-//
-//   if (arr.length > 1) {
-//     for (let i = arr.length - 1; i >= 0; i--) {
-//       if (arr[i] === el[0]) return i
-//     }
-//   }
-//   // if (arr.length === 1) {
-//   //   for (let i = arr.length - 1; i >= 0; i--) {
-//   //     if (arr[i] === el) return i
-//   //   }
-//   // }
-//
-//   // return lastElementIndex
-//   return -1
-// }
-// const stroke = 'Hello World'
-// console.log(lastIndexOfMethod(stroke, 'World'))
-// console.log(stroke.lastIndexOf('World', 6))
+})();
