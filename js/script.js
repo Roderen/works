586199e586051930a54bb21c0e4d4da69fcb8520
Roderen <mikilikala@gmail.com>
2023-05-08 10:20:05 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 "use strict";
 (function () {
+  const attrList = document.querySelector('.ulClass');
+  const lastItem = document.querySelector('.ulClass li:last-child');
+  const firstItem = document.querySelector('.ulClass li:first-child');
 
+  // #1
+  const arrAttrValues = [];
+  const arrAttrNames = [];
+  for (let i of attrList.attributes) {
+    arrAttrValues.push(i.nodeValue);
+    arrAttrNames.push(i.nodeName);
+  }
+  console.log(arrAttrValues);
+  console.log(arrAttrNames);
+
+  // #2
+  lastItem.innerText = 'Привет, меня зовут Максим';
+
+  // #3
+  firstItem.setAttribute('data-my-name', 'maksym');
+
+  // #4
+  attrList.removeAttribute('data-dog-tail');
 })();
