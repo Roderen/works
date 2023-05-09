7366f518656cbd48488248547a0d35329d54c291
Roderen <mikilikala@gmail.com>
2023-05-09 12:47:27 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 "use strict";
 (function () {
+  const createTable = () => {
+    const table = document.createElement("table");
+    const tableBody = document.createElement("tbody");
 
+    for (let i = 0; i < 10; i++) {
+      const tableTr = document.createElement('tr');
+
+      for (let j = 1; j < 11; j++) {
+        const tableTd = document.createElement('td');
+        const tableText = document.createTextNode(`${i * 10 + j}`);
+        tableTd.appendChild(tableText);
+        tableTr.appendChild(tableTd);
+      }
+      tableBody.appendChild(tableTr);
+    }
+    table.appendChild(tableBody);
+    return table;
+  }
+  document.body.appendChild(createTable());
 })();
