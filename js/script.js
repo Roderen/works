db1c5c11be72c8e78a5e91efec016ae45a483295
Roderen <mikilikala@gmail.com>
2023-05-14 17:28:28 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-"use strict";
+'use strict';
 (function () {
+  const form = document.querySelector('#form');
 
-})();
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const result = {};
+
+    const input = form.querySelectorAll('.form-control');
+    const select = form.querySelector('.form-select');
+    const checkbox = form.querySelector('.form-check-input');
+    input.forEach((item) => {
+      if (item.value === '') return;
+      result[item.name] = item.value;
+    });
+
+    if (select) {
+      result[select.name] = select.options[select.selectedIndex].text;
+    }
+
+    if (checkbox) {
+      if (checkbox.checked) result[checkbox.name] = checkbox.value;
+    }
+    console.log(result);
+  });
+}());
