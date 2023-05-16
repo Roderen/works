ab86b714487c9092cf6f247bf733c2df43f8bd73
Roderen <mikilikala@gmail.com>
2023-05-16 22:14:00 +0300

    Modified list of extensions eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": "eslint:recommended",
-  "overrides": [],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
-  "rules": {
+  rules: {
     "semi": ["error", "always"],
     "quotes": ["error", "double"]
-  }
-}
+  },
+};
