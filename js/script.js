0b45ed6b0428a222b846d6c71f64700ad5c6523c
Roderen <mikilikala@gmail.com>
2023-05-19 13:32:59 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,26 +1,45 @@
 'use strict';
 (function () {
   const form = document.querySelector('#form');
+  const dataKey = 'formData';
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const result = {};
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const result = {};
 
-    const input = form.querySelectorAll('.form-control');
-    const select = form.querySelector('.form-select');
-    const checkbox = form.querySelector('.form-check-input');
-    input.forEach((item) => {
-      if (item.value === '') return;
-      result[item.name] = item.value;
+      const input = form.querySelectorAll('.form-control');
+      const select = form.querySelector('.form-select');
+      const checkbox = form.querySelector('.form-check-input');
+      input.forEach((item) => {
+        if (item.value === '') return;
+        result[item.name] = item.value;
+      });
+
+      if (select) {
+        result[select.name] = select.options[select.selectedIndex].text;
+      }
+
+      if (checkbox) {
+        if (checkbox.checked) result[checkbox.name] = checkbox.value;
+      }
+      localStorage.setItem(dataKey, JSON.stringify(result));
     });
+  }
 
-    if (select) {
-      result[select.name] = select.options[select.selectedIndex].text;
-    }
+  document.addEventListener('DOMContentLoaded', () => {
+    const formResult = document.querySelector('.form-result');
 
-    if (checkbox) {
-      if (checkbox.checked) result[checkbox.name] = checkbox.value;
+    if (formResult) {
+      if (!localStorage.getItem(dataKey)) return alert('No data');
+      const result = JSON.parse(localStorage.getItem(dataKey));
+      const ul = document.createElement('ul');
+      Object.entries(result).forEach(([key, value]) => {
+        const li = document.createElement('li');
+        li.innerText = `${key}: ${value}`;
+        ul.appendChild(li);
+      });
+      formResult.appendChild(ul);
     }
-    console.log(result);
   });
 }());
