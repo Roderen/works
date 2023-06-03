76adad419cb3b202fc43eb298519cab6d526769c
Roderen <mikilikala@gmail.com>
2023-06-03 18:08:10 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 "use strict";
 (function () {
+  const user = {
+    data: {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: {
+        a1: 1,
+        b1: 2,
+        c1: 3,
+        d1: {
+          a2: 3,
+          b2: 3,
+          c2: 3,
+        },
+      },
+    },
+  };
 
-})();
+  const deepFreeze = (data) => {
+    Object.keys(data).forEach((item) => {
+      if (typeof item === "object" && item !== null) deepFreeze(item);
+    });
+    return Object.freeze(data);
+  };
+  deepFreeze(user);
+}());
