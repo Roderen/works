aa0de28ed85ac2fe61b0e8dc79866e8c29a6a76d
Roderen <mikilikala@gmail.com>
2023-06-24 20:38:56 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 "use strict";
 (function () {
+  const data = ["a", "b", "c"];
 
-})();
+  function MyIterator(arr) {
+    let index = 0;
+    let obj = {
+      value: undefined,
+      done: false,
+    };
+
+    function iter() {
+      if (index < arr.length) {
+        obj.value = arr[index++];
+        return obj;
+      }
+      obj = {
+        value: undefined,
+        done: false,
+      };
+      return obj;
+    }
+
+    return iter;
+  }
+
+  const result = MyIterator(data);
+  console.log(result());
+  console.log(result());
+  console.log(result());
+  console.log(result());
+}());
