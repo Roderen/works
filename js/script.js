6b6fdbc2ba74367830f55025b57eaab644409ae0
Roderen <mikilikala@gmail.com>
2023-06-30 01:22:58 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 "use strict";
 (function () {
+  const student = { name: 'Иван Иванов' };
+  const grades = { math: 90, fiz: 85, eng: 95 };
 
+  const saveGrades = (person, scores) => {
+    const weakMap = new WeakMap();
+
+    weakMap.set(person, scores);
+
+    return weakMap;
+  }
+  console.log(saveGrades(student, grades));
 })();
