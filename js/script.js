bbd9e2837417b8d5a4453e6a2c7437863cb95233
Roderen <mikilikala@gmail.com>
2023-06-30 00:44:10 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 "use strict";
 (function () {
+  const goods = [['футболка', 15], ['шорты', 25], ['носки', 5]];
 
+  const totalCost = (arr) => {
+    const map = new Map();
+    let total = 0;
+
+    for (let [product, price] of arr) {
+      map.set(product, price);
+    }
+
+    map.forEach((value) => total += value);
+
+    return total;
+  }
+
+  console.log(totalCost(goods));
 })();
