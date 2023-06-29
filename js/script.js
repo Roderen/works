b5a2d46a80aa344967e287144b018738d210941e
Roderen <mikilikala@gmail.com>
2023-06-30 00:53:09 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 "use strict";
 (function () {
+  const string = 'abracadabra';
 
+  const uniqSymbolsCount = (str) => {
+    const set = new Set();
+
+    for (let i = 0; i < str.length; i++) {
+      set.add(str[i]);
+    }
+
+    return set.size;
+  }
+  console.log(uniqSymbolsCount(string));
 })();
