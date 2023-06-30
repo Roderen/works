600ce33b211e7f2ffcb57b721b521f5c57c77fb8
Roderen <mikilikala@gmail.com>
2023-06-30 14:33:33 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 "use strict";
 (function () {
+  const player1 = { id: 1, name: 'Иван', level: 10 };
 
+  const addPlayerToLevelSet = (obj) => {
+    const ws = new WeakSet();
+    ws.add(obj);
+
+    return ws;
+  }
+  console.log(addPlayerToLevelSet(player1));
 })();
