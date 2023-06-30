61b586bcb621c56b66f024e2242d38b2f1d28d5c
Roderen <mikilikala@gmail.com>
2023-06-30 14:45:27 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 "use strict";
 (function () {
+  const students = [
+    { name: 'John', age: 20, subject: 'Math' },
+    { name: 'Alice', age: 22, subject: 'English' },
+    { name: 'Bob', age: 21, subject: 'History' }
+  ];
 
+  const printGreetings = (arr) => {
+    for (let {name, subject} of arr) {
+      const str = `Привет, ${name} John! Ты изучаешь предмет ${subject}`
+      console.log(str);
+    }
+  }
+
+  printGreetings(students);
 })();
