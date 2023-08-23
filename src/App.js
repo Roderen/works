701096f6c3992c673b49a6ce4a9c733e943b2218
Roderen <mikilikala@gmail.com>
2023-08-23 15:11:42 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react"
+import Card from "./components/Card"
 
 class App extends React.Component {
   render() {
     return (
         <div className="App">
-
+          <Card title={"First title"}/>
+          <Card text={"Second Text"}/>
+          <Card
+              title={"Third title"}
+              text={"Third text"}
+          />
         </div>
     )
   }
