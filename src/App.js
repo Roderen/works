24bc04d1c20d30ae1d00af1dc90ae4b070a2b235
Roderen <mikilikala@gmail.com>
2023-09-09 18:28:25 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
+import Component from "./components/Component";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
     return (
         <div className="App">
-
+          <Component />
         </div>
     )
   }
--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -1,8 +1,63 @@
 import React from "react"
+import {v4 as uuidv4} from 'uuid';
 
 class Component extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logs: []
+    }
+  }
 
+  plusLog = () => {
+    const {logs} = this.state;
+    const lastEl = logs.at(0)?.value ?? 0;
+    this.setState({
+      logs: [{
+        id: uuidv4(),
+        value: lastEl + 1
+      }, ...logs]
+    });
+  }
+
+  minusLog = () => {
+    const {logs} = this.state;
+    const lastEl = logs.at(0)?.value ?? 0;
+    this.setState({
+      logs: [{
+        id: uuidv4(),
+        value: lastEl - 1
+      }, ...logs]
+    });
+  }
+
+  removeLogFromList = (id) => {
+    const {logs} = this.state;
+    const result = logs.findIndex((item) => item.id === id);
+    this.setState(logs.splice(result, 1))
+  }
+
+  render() {
+    return <>
+      <div>
+        <div className="btn-group font-monospace" role="group">
+          <button type="button" className="btn btn-outline-success" onClick={this.plusLog}>+</button>
+          <button type="button" className="btn btn-outline-danger" onClick={this.minusLog}>-</button>
+        </div>
+        <div className="list-group">
+          {this.state.logs.map(({id, value}) => (
+              <button
+                  key={id}
+                  type="button"
+                  className="list-group-item list-group-item-action"
+                  onClick={() => this.removeLogFromList(id)}
+              >
+                {value}
+              </button>
+          ))}
+        </div>
+      </div>
+    </>
   }
 }
 
